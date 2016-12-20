4761917a5d60d421f2cc8731df0fa03fb08057ca
Tim Zöller <tim.zoeller@ilume.de>
2016-12-20 15:34:10 +0100

    Completed 'lights' API

--- src/farbton/lights.clj
+++ src/farbton/lights.clj
@@ -6,51 +6,62 @@
   ([username hostname appendix]
    (str "http://" hostname "/api/" username "/lights/" appendix))
   ([username hostname]
-    (str "http://" hostname "/api/" username "/lights")
-    ))
+    (str "http://" hostname "/api/" username "/lights")))
 
+(defn search-for-new-lights
+  "Scans the network for new devices"
+  [username {ip-address :internalipaddress}]
+  (:body (client/post (build-uri username 
+                                 ip-address) 
+                      {:as :json})))
+ 
+(defn request-new-lights
+  "Returns a list of lights which are found during the last scan"
+  [username {ip-address :internalipaddress}]
+  (:body (client/get (build-uri username 
+                                ip-address 
+                                "new") 
+                             {:as :json})))
+
+(defn rename-light
+  "Sets a new name for the given light"
+  [username {ip-address :internalipaddress} light name]
+  (:body (client/put (build-uri username 
+                                ip-address 
+                                light) 
+                     {:form-params {:name name} 
+                      :content-type :json
+                      :as :json})))
 
 (defn request-lights
   "Returns a list of available lights"
   [username {ip-address :internalipaddress}]
-  (let [response (client/get (build-uri username ip-address) 
-                             {:as :json})]
-    (:body response)))
+  (:body (client/get (build-uri username 
+                                ip-address) 
+                     {:as :json})))
 
 (defn request-light
   "Returns information about the queried light"
   [username {ip-address :internalipaddress} light]
-  (let [response (client/get (build-uri username ip-address light) 
-                             {:as :json})]
-    (:body response)))
+  (:body (client/get (build-uri username 
+                                ip-address 
+                                light) 
+                     {:as :json})))
 
 (defn change-light-state
   "Changes the state of the light"
   [username {ip-address :internalipaddress} light params]
-  (let [response (client/put (build-uri username ip-address (str light "/state")) 
-                             {:form-params params
-                              :content-type :json
-                              :as :json})]
-    (:body response)))
+  (:body (client/put (build-uri username 
+                                ip-address 
+                                (str light "/state")) 
+                     {:form-params params
+                      :content-type :json
+                      :as :json})))
 
-(defn switch-all-on
-  "Switches all available lamps on. Not sure, if useful"
-  [username bridge]
-  (let [lights (request-lights username bridge)
-        all-light-ids (map #(name (first %)) lights)]
-    (pmap #(change-light-state username 
-                              bridge 
-                              % 
-                              {:on true}) 
-         all-light-ids)))
-
-(defn switch-all-off
-  "Switches all available lamps off. Not sure, if useful"
-  [username bridge]
-  (let  [lights (request-lights username bridge)
-        all-light-ids (map #(name (first %)) lights)]
-    (pmap #(change-light-state username
-                              bridge
-                              %
-                              {:on false})
-         all-light-ids)))
+(defn delete-light
+  "Deletes a light. Use with caution!"
+  [username {ip-address :internalipaddress} light]
+  (:body (client/delete (build-uri username
+                                   ip-address
+                                   light)
+                        {:as :json})))
